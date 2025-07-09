16169dffcee399961632220aa9fd936c95729f78
Foco820 <2156914087@qq.com>
2025-07-09 14:14:14 +0800

    IngredientType
    making Ingredient Scripts

---
+++ SpaceKitchen/Assets/Scripts/Ingredient/Ingredient.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    public IngredientType currentType;           //当前食物类型
+    public string displayName;                   //显示名称
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //名称显示
+        UpdateDisplayName();
+    }
+
+    public void UpdateDisplayName()
+    {
+        //根据类型显示名称
+        switch (currentType)
+        {
+            case IngredientType.RawProk:
+                displayName = "生猪肉";
+                break;
+            case IngredientType.PorkChop:
+                displayName = "猪排";
+                break;
+            case IngredientType.MincedPork:
+                displayName = "猪肉糜";
+                break;
+            case IngredientType.CookedPork:
+                displayName = "烤猪肉";
+                break;
+            case IngredientType.CookedPorkChop:
+                displayName = "烤猪排";
+                break;
+        }
+
+        //更新UI
+        GetComponentInChildren<TextMesh>().text = displayName;
+    }
+}
---
+++ SpaceKitchen/Assets/Scripts/Ingredient/IngredientType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ≥≤ƒ÷÷¿‡
+public enum IngredientType
+{
+    RawProk,                     //…˙÷Ì»‚
+    PorkChop,                    //÷Ì≈≈
+    MincedPork,                  //÷Ì»‚√”
+    CookedPork,                  //øæ÷Ì»‚
+    CookedPorkChop               //øæ÷Ì≈≈
+}
